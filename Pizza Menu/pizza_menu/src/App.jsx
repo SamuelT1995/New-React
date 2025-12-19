e1702d561cea2582daff2b88e824dbb82e22f5a6
SamuelT1995 <samueltenagne@gmail.com>
2025-12-19 10:41:00 +0300

    basic components

--- Pizza Menu/pizza_menu/src/App.jsx
+++ Pizza Menu/pizza_menu/src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Pizza from "./Pizza";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
 };
 
 export default App;
