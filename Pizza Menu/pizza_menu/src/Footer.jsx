25ded85a6296718e9cf548d6c83d56bace44a521
SamuelT1995 <samueltenagne@gmail.com>
2025-12-22 09:48:16 +0300

    steps init

--- Pizza Menu/pizza_menu/src/Footer.jsx
+++ Pizza Menu/pizza_menu/src/Footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
     <div>
-      <footer>{new Date().toLocaleTimeString()} We're currently Open</footer>
+      {isOpen && (
+        <div className="order">
+          <p>we're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </div>
   );
 };
