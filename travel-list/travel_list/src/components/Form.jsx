2ca16c36cf075c1dfdf9e5af476042ad74d0af88
SamuelT1995 <samueltenagne@gmail.com>
2025-12-25 16:57:50 +0300

    continued

---
+++ travel-list/travel_list/src/components/Form.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>what do you need for your trip</h3>
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item....."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default Form;
