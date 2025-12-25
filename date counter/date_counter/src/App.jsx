60d489b5fbeb5a966445adb5510b943fbe745761
SamuelT1995 <samueltenagne@gmail.com>
2025-12-25 06:27:28 +0300

    complete

--- date counter/date_counter/src/App.jsx
+++ date counter/date_counter/src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date("Jan 21 2028");
+  date.setDate(date.getDate() + count);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <span>Steps: {step}</span>
+        <button onClick={() => setStep((s) => s - 1)}>- </button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+      </div>
+      <p>
+        <span>
+          {count == 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from today`
+            : `${Math.abs(count)} days ago is`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
 };
 
 export default App;
